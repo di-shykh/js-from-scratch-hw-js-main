85fe1ef0e9f3d87bfbb7f99ecfbe84bd84311e80
di-shykh <di49@mail.ru>
2025-09-02 13:05:30 +0300

    fix: fix task 03 lesson 3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -2,12 +2,14 @@
 
 function findLargest(a, b, c) {
   //return Math.max(a, b, c);
-  let max = a;
-  if (b - a >= 0) {
-    max = b;
-  }
-  if (c - b >= 0) {
-    max = c;
-  }
+  // let max = a;
+  // if (b - a >= 0) {
+  //   max = b;
+  // }
+  // if (c - b >= 0) {
+  //   max = c;
+  // }
+  let max = a > b ? a : b;
+  max = b > c ? b : c;
   return max;
 }
